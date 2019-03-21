933e9d93b963d6b5651edbea8cd54898791a8954
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-03-21 14:10:07 +0100

    [DEV] SEOS-90: add OSAL (OS Abstaction Layer)

---
+++ include/lib_osal/System.h
@@ -0,0 +1,45 @@
+#if !defined(SYSTEM_H)
+#define SYSTEM_H
+
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <stdbool.h>
+
+/** This also defines
+ * System_BEGIN_CRITICAL_SECTION
+ * System_END_CRITICAL_SECTION
+ *
+ */
+#include "lib_osal/System_Impl.h"
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+unsigned long
+System_getTicksPerMs(unsigned long ms);
+
+unsigned long long
+System_getTickCount(void);
+
+unsigned long long
+System_upTimeMs(void);
+
+/**
+ * @brief delay in Millis, ms maybe translated in tick thru an integer division
+ * in this case if div result is == 0 the behaviour is not specified and impl
+ * dependent
+ * @see System_safeDlyMs
+ */
+void
+System_delayMs(unsigned long ms);
+
+void
+System_delayTicks(unsigned long ticks);
+
+
+#endif
+
---
+++ include/lib_osal/System_Impl.h
@@ -0,0 +1,35 @@
+#ifndef SYSTEM_IMPL_H
+#define SYSTEM_IMPL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+INLINE unsigned long long
+System_getTickCount(void)
+{
+    /// not implemented
+    Debug_ASSERT(false);
+}
+
+INLINE unsigned long long
+System_upTimeMs(void)
+{
+    /// not implemented
+    Debug_ASSERT(false);
+
+}
+
+INLINE void
+System_delayTicks(unsigned long ticks)
+{
+    /// not implemented
+    Debug_ASSERT(false);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTEM_IMPL_H */
+
