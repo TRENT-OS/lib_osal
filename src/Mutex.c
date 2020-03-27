ded61c60e384e0181661fe098483b7846b938e70
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2020-03-27 11:42:02 +0100

    [DEV] rename 'seos' folder in 'OS' as seos is obsolete

---
+++ src/Mutex.c
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (C) 2019, Hensoldt Cyber GmbH
+ */
+#include "lib_osal/Mutex.h"
+#include "lib_debug/Debug.h"
+
+bool
+Mutex_ctor(Mutex* self)
+{
+    return true;
+}
+
+void
+Mutex_dtor(Mutex* self)
+{
+    return;
+}
+
+void
+Mutex_acquire(Mutex* self)
+{
+    int err = self->lock();
+    Debug_ASSERT(!err);
+}
+
+void
+Mutex_release(Mutex* self)
+{
+    int err = self->unlock();
+    Debug_ASSERT(!err);
+}
+
+bool
+Mutex_tryAcquire(Mutex* self)
+{
+    // not implemented
+    Debug_ASSERT(false);
+}
