c5a0ff33370005594c0ff8bbeffc3e77bbff59cb
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-21 17:02:30 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- src/Mutex.c
+++ src/Mutex.c
@@ -1,6 +1,11 @@
 /*
- *  Copyright (C) 2019, HENSOLDT Cyber GmbH
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
+
 #include "lib_osal/Mutex.h"
 #include "lib_compiler/compiler.h"
 #include "lib_debug/Debug.h"
